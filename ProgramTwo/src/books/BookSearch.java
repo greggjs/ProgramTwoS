5815b7623f59b83bcaff34992ca534835a264729
Jake Gregg <greggjs@muohio.edu>
2012-09-14 13:37:11 -0400

    Added the BookSearch changes
    Thanks Eric for starting that

--- ProgramTwo/src/books/BookSearch.java
+++ ProgramTwo/src/books/BookSearch.java
@@ -1,5 +1,26 @@
 package books;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class BookSearch {
+
+	static Connection conn;
+	static Statement stat;
+	static ResultSet rs;
+
+	// Static entry-point
+	public static void main(String args[]) {
+
+	}
+
+	// Used by JDBC to connect to the assigned database.
+	public void connect() throws Exception {
+		Class.forName("org.sqlite.JDBC");
+		conn = DriverManager.getConnection("jdbc:sqlite:Assignment3.db");
+		stat = conn.createStatement();
+	}
 
 }
