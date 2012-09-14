3dcd4b57cc31eac33f503803d5221062126323cf
Shannon Kirby <eric_k_11@yahoo.com>
2012-09-14 13:19:37 -0400

    Removed BookPanel, updated Author based on Kyle's work, Made Book
    Also started work on BookSearch

--- ProgramTwo/src/books/Book.java
+++ ProgramTwo/src/books/Book.java
@@ -1,13 +1,65 @@
 package books;
 
-public class Book {
+import java.sql.*;
+
+public class Book implements Comparable<Book>	{
+
+	private static final String
+	SQL_TITLE = "title",
+	SQL_PUBLISH_MONTH = "pMonth",
+	SQL_PUBLISH_DAY = "pDay",
+	SQL_PUBLISH_YEAR = "pYear",
+	SQL_BOOK_ID = "bookID";
+
+	private String title;
+	private int pMonth, pDay, pYear;
+	private int bookID;
 
 	/**
-	 * @param args
+	 * Creates an object of type Book
+	 * @param rs A ResultSet containing a Book entry in a database
+	 * @throws SQLException May result from the ResultSet not containing an entry for Book
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public Book(ResultSet rs) throws SQLException	{
+		title = rs.getString(SQL_TITLE);
+		pDay = rs.getInt(SQL_PUBLISH_DAY);
+		pMonth = rs.getInt(SQL_PUBLISH_MONTH);
+		pYear = rs.getInt(SQL_PUBLISH_YEAR);
+		bookID = rs.getInt(SQL_BOOK_ID);
+	}
 
+	/**
+	 * Formated publish date for book
+	 * @return Publish date of book in format: 1-2-1990 (M-D-Y)
+	 */
+	public String getPublishDate()	{
+		return pMonth + "-" + pDay + "-" + pYear;
+	}
+
+	/**
+	 * Formats author information to be used for an SQL entry. does not include parentheses
+	 * Includes NULL for autoincrementing primary key bookID
+	 * @return String formated for SQL use
+	 */
+	public String toSQLString()	{
+		return title + "," + pDay + "," + pMonth + "," + pYear + ", NULL";
+	}
+
+	/**
+	 * Creates a string representation of Author
+	 * @return Formated string
+	 */
+	public String toString()	{
+		return "Title: " + title + " -- Published: " + getPublishDate();
+	}
+
+	/**
+	 * Compares to another book's title alphabetically
+	 * @return
+	 */
+	public int compareTo(Book arg0) {
+		if (!title.equalsIgnoreCase(arg0.title)) return title.compareToIgnoreCase(arg0.title);
+		return bookID - arg0.bookID;
 	}
 
 }
--- ProgramTwo/src/books/BookSearch.java
+++ ProgramTwo/src/books/BookSearch.java
@@ -1,13 +1,27 @@
 package books;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class BookSearch {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	static Connection conn;
+	static Statement stat;
+	static ResultSet rs;
+	
+	//Static entry-point
+	public static void main(String args[]) {
+		
+	}
 
+	//Used by JDBC to connect to the assigned database.
+	public void connect() throws Exception{
+		Class.forName("org.sqlite.JDBC");
+		conn =
+				DriverManager.getConnection("jdbc:sqlite:Assignment3.db");
+		stat = conn.createStatement();
 	}
 
 }
